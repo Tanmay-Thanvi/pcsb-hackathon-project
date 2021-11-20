4ee1c0d6e63c30a1cde4a3860e709dec7fc04249
Tanmay-Thanvi <78643125+Tanmay-Thanvi@users.noreply.github.com>
2021-11-20 22:48:35 +0530

    Delete pcsb_hack directory

--- pcsb_hack/SIG/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SIG,coursecontent,announcement,classroom
-# Register your models here.
-admin.site.register(SIG)
-admin.site.register(coursecontent)
-admin.site.register(announcement)
-admin.site.register(classroom)
